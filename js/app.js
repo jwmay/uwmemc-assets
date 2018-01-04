0f4990a78ada94ad54b24e476d1ee513df9d27e2
Joseph May <jwmay87@gmail.com>
2018-01-03 22:46:13 -0800

    use FontAwesome 5 Pro

--- js/app.js
+++ js/app.js
@@ -1,11 +1,18 @@
+// Load jQuery and WhatInput (used by Zurb Foundation)
 import $ from 'jquery';
 import whatInput from 'what-input';
 
 window.$ = $;
 
+// Initialize Zurb Foundation
 import './lib/foundation-explicit-pieces';
-
 $(document).foundation();
 
+// Initialize FontAwesome
+var font_awesome = require('./lib/fontawesome-all.min');
+
+// Initialize the parallax library
 var parallaxModule = require('./lib/jquery.parallax');
+
+// Initialize other custom JS
 var custom = require('./custom');
